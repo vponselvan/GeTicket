3741bddd37b86efdd01d709f03d5944d8152253d
ScaRvps <58455924+ScaRvps@users.noreply.github.com>
2021-02-24 18:02:08 -0500

    Changed Stan client to protected

--- common/src/events/subscriber.ts
+++ common/src/events/subscriber.ts
@@ -7,15 +7,15 @@ export abstract class Subscriber<T extends Event> {
     abstract queueGroupName: string;
     abstract onMessage(data: T['data'], msg: Message): void;
 
-    protected _client: Stan;
+    protected client: Stan;
     protected ackWait = 5 * 1000; //5Sec 
 
     constructor(client: Stan) {
-        this._client = client;
+        this.client = client;
     }
 
     subscriptionOptions() {
-        return this._client.subscriptionOptions()
+        return this.client.subscriptionOptions()
             .setDeliverAllAvailable()
             .setManualAckMode(true)
             .setDurableName(this.queueGroupName)
@@ -23,7 +23,7 @@ export abstract class Subscriber<T extends Event> {
     }
 
     subscribe() {
-        const subscription = this._client.subscribe(this.subject, this.queueGroupName, this.subscriptionOptions());
+        const subscription = this.client.subscribe(this.subject, this.queueGroupName, this.subscriptionOptions());
         subscription.on('message', (msg: Message) => {
             console.log(`Message Received: ${this.subject}/${this.queueGroupName}`);
 
